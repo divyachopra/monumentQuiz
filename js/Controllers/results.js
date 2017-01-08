8e3086d2c35a9ae6fe4507e4f7c41beea0b00077
divya_chopra <divya.chopra6@ufl.edu>
2017-01-08 12:57:55 -0500

    BootStrap markup for results page

--- js/Controllers/results.js
+++ js/Controllers/results.js
@@ -5,8 +5,10 @@ var resApp= angular.module('trivia');
 
 resApp.controller('resultCtrl', ResultController);
 
-ResultController.$inject = ['quizMetrics'];
-function ResultController(quizMetrics){
+ResultController.$inject = ['quizMetrics','DataService'];
+function ResultController(quizMetrics,DataService){
     var vm = this;
     vm.quizMetrics = quizMetrics;
+    vm.DataService = DataService;
+    vm.activeQuestion=0
 }
